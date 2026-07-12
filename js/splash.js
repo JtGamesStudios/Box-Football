b38289ff9c3f0936400f22c953fc4b9404d5590d
Jt Games Studios <contatojtgamesstudios@gmail.com>
2026-07-12 18:53:53 -0300

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -16,7 +16,7 @@
   // Quando quiser liberar o app de novo, é só voltar para false.
   // ---------------------------------------------------------------
   const MAINTENANCE_MODE = true;
-  const MAINTENANCE_END_LABEL = "domingo, 13/07";
+  const MAINTENANCE_END_LABEL = "Segunda, 13/07";
 
   // ---------------------------------------------------------------
   // ACESSO SECRETO (bypass da manutenção)
