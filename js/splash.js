1079b7a35d5135af58740cf19ed72e9b75393735
Jt Games Studios <contatojtgamesstudios@gmail.com>
2026-07-21 17:21:17 -0300

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -15,8 +15,8 @@
   // Deixe true enquanto o servidor estiver em manutenção.
   // Quando quiser liberar o app de novo, é só voltar para false.
   // ---------------------------------------------------------------
-  const MAINTENANCE_MODE = false;
-  const MAINTENANCE_END_LABEL = "Segunda, 20/07 17:59";
+  const MAINTENANCE_MODE = true;
+  const MAINTENANCE_END_LABEL = "Terça, 21/07 19:00";
 
   // ---------------------------------------------------------------
   // ACESSO SECRETO (bypass da manutenção)
@@ -28,7 +28,7 @@
   // do site ou trocar de dispositivo/navegador.
   // ---------------------------------------------------------------
   const BYPASS_KEY = "pes2021mobile";
-  const BYPASS_STORAGE_FLAG = "boxclube_bypass_maintenance_v7";
+  const BYPASS_STORAGE_FLAG = "boxclube_bypass_maintenance_v8";
 
   function hasBypass() {
     try {
