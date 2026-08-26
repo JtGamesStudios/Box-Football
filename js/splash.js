8113230a958928523be9dcf09861e38bdc9e64f9
Jt Games Studios <contatojtgamesstudios@gmail.com>
2026-08-25 23:04:59 -0300

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -20,7 +20,7 @@
   // Deixe true enquanto o servidor estiver em manutenção.
   // Quando quiser liberar o app de novo, é só voltar para false.
   // ---------------------------------------------------------------
-  const MAINTENANCE_MODE = false;
+  const MAINTENANCE_MODE = true;
   const MAINTENANCE_END_LABEL = "Em breve";
 
   // ---------------------------------------------------------------
